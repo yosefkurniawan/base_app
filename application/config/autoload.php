652c3b4462f4f2d7d0a70686341f7f467f3ea4f8
yosefkurniawan <jojo.maniack@gmail.com>
2015-02-25 22:49:40 +0700

    user CRUD updates using datatables

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session','form_validation','breadcrumb','message');
+$autoload['libraries'] = array('database', 'session','form_validation','breadcrumb','message','table','Datatables');
 
 
 /*
---
+++ application/modules/base/models/setting/user_model.php
@@ -0,0 +1,80 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class User_model extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function get_all($limit, $uri) {
+
+        $result = $this->db->get('core_user', $limit, $uri);
+        if ($result->num_rows() > 0) {
+            return $result->result_array();
+        } else {
+            return array();
+        }
+    }
+    
+    function get_one($user_id) {
+        $this->db->where('user_id', $user_id);
+        $result = $this->db->get('core_user');
+        if ($result->num_rows() == 1) {
+            return $result->row_array();
+        } else {
+            return array();
+        }
+    }
+
+    function save() {
+        $inputs   = $this->input->post('data');
+        
+        $data = array(
+        'user_name' => $inputs['user_name'],
+        'user_pass' => $inputs['user_pass'],
+        'user_email' => $inputs['user_email'],
+        'user_full_name' => $inputs['user_full_name'],
+        'user_address' => $inputs['user_address'],
+        'user_birthday' => $inputs['user_birthday'],
+        'user_picture' => $inputs['user_picture'],
+        'user_phone' => $inputs['user_phone'],
+        'user_st' => $inputs['user_st'],
+        'dc' => date('Y-m-d'),
+        );
+
+        return $this->db->insert('core_user', $data);
+    }
+
+    function update() {
+        $inputs   = $this->input->post('data');
+        $user_id  = $this->input->post('id');
+
+        $data = array(
+        'user_id' => $user_id,
+        'user_name' => $inputs['user_name'],
+        'user_pass' => $inputs['user_pass'],
+        'user_email' => $inputs['user_email'],
+        'user_full_name' => $inputs['user_full_name'],
+        'user_address' => $inputs['user_address'],
+        'user_birthday' => $inputs['user_birthday'],
+        'user_picture' => $inputs['user_picture'],
+        'user_phone' => $inputs['user_phone'],
+        'user_st' => $inputs['user_st'],
+        'du' => date('Y-m-d'),
+        );
+        $this->db->where('user_id', $user_id);
+        return $this->db->update('core_user', $data);
+    }
+
+    function delete() {
+        $user_id  = $this->input->post('id');
+        
+        foreach ($user_id as $row) {
+            $this->db->where('user_id', $row);
+            return $this->db->delete('core_user');
+        }
+    }
+
+}
+?>
